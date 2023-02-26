cbc289aa91c34a7760da7776217b81eaca62f1af
Vinicius2J <jose.vinicius.batista292@gmail.com>
2023-02-25 23:41:18 -0300

    Adicionado aula 02 e aula 01 concluida

---
+++ aulas/aula 01/aula01.js
@@ -0,0 +1,2 @@
+console.log("Vinicios")//Este comando vai escrever no console o nome que esta entre aspas
+
---
+++ aulas/aula 02/aula02.js
@@ -0,0 +1,2 @@
+let idade = 18; //para definar variaveis vocé so precissa digitar "let" o nome da variavel nesse caso que e "idade = o valor dela que nesse caso e 18";
+console.log(idade);
